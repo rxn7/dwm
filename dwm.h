3609526c1b7d649f9bb72ea43986643c4678c50d
rxtthin <rotthin_dev@protonmail.com>
2022-04-13 02:07:21 +0200

    Laptop init

--- dwm.h
+++ dwm.h
@@ -15,7 +15,7 @@
 #include <X11/keysym.h>
 #include <X11/Xatom.h>
 #include <X11/Xlib.h>
-#include <X11/Xproto.h>
+#include <X11/Xproto.h> 
 #include <X11/Xutil.h>
 #ifdef XINERAMA
 #include <X11/extensions/Xinerama.h>
