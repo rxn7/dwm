9eb26dfe353fdbbde3069c6ac50f4fab7be0b88e
Rotthin <rotthin_dev@protonmail.com>
2022-05-03 08:45:01 +0200

    Draw statusbar on all monitors

--- dwm.c
+++ dwm.c
@@ -455,11 +455,11 @@ void drawbar(Monitor *m) {
 		stw = getsystraywidth();
 
 	/* draw status first so it can be overdrawn by tags later */
-	if (m == selmon) { /* status is only drawn on selected monitor */
-		drw_setscheme(drw, scheme[SchemeNorm]);
-		tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
-		drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
-	}
+	// if (m == selmon) { /* status is only drawn on selected monitor */
+	drw_setscheme(drw, scheme[SchemeNorm]);
+	tw = TEXTW(stext) - lrpad / 2 + 2; /* 2px extra right padding */
+	drw_text(drw, m->ww - tw - stw, 0, tw, bh, lrpad / 2 - 2, stext, 0);
+	// }
 
 	resizebarwin(m);
 	for (c = m->clients; c; c = c->next) {
